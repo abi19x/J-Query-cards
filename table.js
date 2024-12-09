ab588b66c01e29ab8b7fcbc2f8b250e3a8f102eb
Abel Yohanes <k9ja1xwk@students.codeinstitute.net>
2024-12-09 16:37:05 +0000

    modified the script file

--- table.js
+++ table.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     $("h2").addClass("underline"); //underlines all <h2> elements
-    $("ul").removeClass("border"); //adds border to all unordered lists
+    $("ul").removeClass("border"); //removes border from all unordered lists
 });
